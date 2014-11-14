ebc81461f553dc2dd0796ec77d00004170ae5206
geekan <geekan@foxmail.com>
2014-11-14 23:07:27 +0800

    it works!

--- test.py
+++ test.py
@@ -1,12 +1,29 @@
 #!/usr/bin/env python
 
 import urllib
+from multiprocessing import Pool
+
+results = []
+def callback(result):
+    results.append(result)
 
 def main():
+    pool = Pool(processes=36)
     with open('urls.log') as f:
-        for line in f:
-            print line.split()
-    # urllib.urlretrieve("http://3gimg.qq.com/wap30/info/info5/img/newshare.png", "2.png")
+        for index, line in enumerate(f):
+            try:
+                count, url = line.split()
+            except:
+                print 'exception:', count, url
+                continue
+            print count, url
+            result = pool.apply_async(
+                    urllib.urlretrieve,
+                    args=(url, 'imgs/'+str(index)+'.'+count+'.'+url.split('/')[-1]),
+                    # callback=callback
+            )
+    pool.close()
+    pool.join()
 
 if __name__ == '__main__':
     main()
