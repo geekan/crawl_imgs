8993dbb4d4c3e3d3443558e8d59749af5df5d4f0
geekan <geekan@foxmail.com>
2014-11-18 23:03:25 +0800

    add test-white

---
+++ test-white.py
@@ -0,0 +1,33 @@
+import os
+import cv2
+import numpy as np
+
+def parse(filename):
+    img = cv2.imread(filename)
+    if img is None:
+        os.remove(filename)
+        print filename, 'is none'
+        return
+
+    lower_color = np.array([250, 250, 250])
+    upper_color = np.array([255, 255, 255])
+
+    print filename
+    print img.size
+    dst = cv2.inRange(img, lower_color, upper_color)
+
+    cv2.imshow('img', img)
+    cv2.imshow('dst', dst)
+
+    print cv2.countNonZero(dst)
+
+    cv2.waitKey(0)
+
+files = os.listdir('./imgs')
+def main():
+    for f in files:
+        parse('imgs/' + f)
+    return
+
+if __name__ == '__main__':
+    main()
