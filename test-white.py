239db207690cdb0181537817d327c04bf2f1b14f
geekan <geekan@foxmail.com>
2014-11-19 10:16:11 +0800

    update

--- test-white.py
+++ test-white.py
@@ -9,6 +9,7 @@ def parse(filename):
         print filename, 'is none'
         return
 
+    return
     lower_color = np.array([250, 250, 250])
     upper_color = np.array([255, 255, 255])
 
@@ -16,12 +17,12 @@ def parse(filename):
     print img.size
     dst = cv2.inRange(img, lower_color, upper_color)
 
-    cv2.imshow('img', img)
-    cv2.imshow('dst', dst)
+    #cv2.imshow('img', img)
+    #cv2.imshow('dst', dst)
 
     print cv2.countNonZero(dst)
 
-    cv2.waitKey(0)
+    #cv2.waitKey(0)
 
 files = os.listdir('./imgs')
 def main():
